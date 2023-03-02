7e5432650ed5c18d74795bd4c1c22d2b181ba85e
Jon Chen <jchen2190@gmail.com>
2023-03-01 19:48:16 -0500

    blur the backdrop

---
+++ riddles.js
@@ -0,0 +1,15 @@
+const riddles = [ 
+
+    { riddle: "What has to be broken before you can use it?", answer: "egg" },
+    { riddle: "I’m tall when I’m young, and I’m short when I’m old. What am I?", answer: "candle" },
+    { riddle: "What is full of holes but still holds water?", answer: "sponge" },
+    { riddle: "What is always in front of you but can’t be seen?", answer: "future" },
+    { riddle: "What can you break, even if you never pick it up or touch it?", answer: "promise" },
+    { riddle: "What goes up but never comes down?", answer: "age" },
+    { riddle: "What gets wet while drying?", answer: "towel" },
+    { riddle: "What can’t talk but will reply when spoken to?", answer: "bank" },
+    { riddle: "I have branches, but no fruit, trunk or leaves. What am I?", answer: "echo" },
+    
+    // { riddle: "What month of the year has 28 days?", answer: "all", answer2: "every month" },
+    // { riddle: "What can you keep after giving to someone?", answer: "word", answer2: "your word" },
+];
